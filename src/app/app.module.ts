0e1963ae028769f67067953daf86bc00b149e314
Patrick <patrick.wolfs@netintellect.be>
2019-11-03 18:07:34 +0100

    example menu with left & right items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ExampleLayoutComponent } from './examples/layout/example-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleLayoutComponent
   ],
   imports: [
     BrowserModule,
